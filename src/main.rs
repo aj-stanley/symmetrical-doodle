44de3faed49fd1347c0b6f601d751b6c7e2cfac2
aj.stanley <stan@math.ai>
2025-06-11 18:12:02 +0530

    chore; add shared state

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 use ntex::web;
 
+struct AppState {
+    app_name: String,
+}
+
 #[web::get("/")]
 async fn hello() -> impl web::Responder {
-    web::HttpResponse::Ok().body("Hello World!")
+    web::HttpResponse::Ok().body("Hello!")
+}
+#[web::get("/st/{name}")]
+async fn hello_name(data: web::types::State<AppState>, req: web::types::Path<String>) -> String {
+    let name = req.into_inner();
+    let app_name = &data.app_name;
+    format!("Hello {app_name}!, {name}!")
 }
 
 #[web::post("/echo")]
@@ -10,17 +20,21 @@ async fn echo(req_body: String) -> impl web::Responder {
     web::HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_hello() -> impl web::Responder {
-    web::HttpResponse::Ok().body("Hey there!")
+async fn hey() -> impl web::Responder {
+    web::HttpResponse::Ok().body("Hey!")
 }
 
 #[ntex::main]
 async fn main() -> std::io::Result<()> {
     web::HttpServer::new(|| {
         web::App::new()
-            .service(hello)
+            .service(web::scope("/api").service(hello))
+            .state(AppState {
+                app_name: String::from("Ntex"),
+            })
+            .service(hello_name)
             .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .route("/hey", web::get().to(hey))
     })
     .bind("127.0.0.1:8080")?
     .run()
